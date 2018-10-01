2171e4e6894340d438b472f74df42a00482c2ba5
Schtoo <mgqobhoz@gmail.com>
2018-10-01 16:55:37 +0200

    added query selector for getting foreign key

--- reg.js
+++ reg.js
@@ -1,35 +1,27 @@
 module.exports = function (pool) {
+  // Adding the registration number
   async function takeRegNumber(regPlate ) {
     let whichTown = regPlate.substr(0, 3).trim();
     let result = await pool.query('SELECT * FROM plates WHERE registration=$1', [whichTown]);
     if (result.rowCount === 0) {
       let townId = await pool.query('SELECT id FROM towns WHERE starts_with=$1', [whichTown]);
-      //console.log(townId.rows[0].id)
       await pool.query('INSERT INTO plates (registration, towns_id) values ($1, $2)', [regPlate, townId.rows[0].id]);
     }
   }
-
+  //Get registration
   async function getRegPlate() {
     let getPlates = await pool.query('SELECT * FROM plates');
     return getPlates.rows;
   }
-
-  // async function getTown(pickTown) {
-  //   let towns = await pool.query('SELECT FROM towns town_name, starts_with');
-  //   for (let i = 0; i < towns.rowCount; i++) {
-  //     let storedTowns = towns.rows[i];
-  //     if (storedTowns.starts_with === pickTown) {
-  //       storedTowns.selected = true;
-  //     }
-  //   }
-  //   return towns.rows;
-  // }
-
-
+//reset database 
+  async function resetDb() {
+    let reset = await pool.query('DELETE FROM plates');
+    return reset;
+  }
 
   return {
     takeRegNumber,
-    // getTown,
+    resetDb,
     getRegPlate
   }
 }
