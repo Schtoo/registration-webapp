ecd57cd66e5073135574ea794f22e236fee9c928
Schtoo <mgqobhoz@gmail.com>
2018-10-05 16:00:33 +0200

    added a function for filtering for a town selected

--- reg.js
+++ reg.js
@@ -1,6 +1,6 @@
 module.exports = function (pool) {
   // Adding the registration number
-  async function takeRegNumber(regPlate ) {
+  async function takeRegNumber(regPlate) {
     let whichTown = regPlate.substr(0, 3).trim();
     let result = await pool.query('SELECT * FROM plates WHERE registration=$1', [whichTown]);
     if (result.rowCount === 0) {
@@ -18,17 +18,33 @@ module.exports = function (pool) {
     let reset = await pool.query('DELETE FROM plates');
     return reset;
   }
-//filter Functions for towns
+//Filter Function for all towns
   async function forTowns() {
-    let cape = await pool.query("SELECT * FROM plates");
-    console.log(cape.rows);
-    return cape.rows;
+    let city = await pool.query("SELECT * FROM towns");
+   //console.log(city.rows);
+    return city.rows;
+  }
+
+  async function allRegNumbers () {
+    let regNumbs = await pool.query('SELECT * FROM plates');
+    return regNumbs.rows;
+  }
+//Filtering for specific town
+  async function townFilter (regNo) {
+    let regTown = regNo.substr(0, 3).trim();
+    let result = await pool.query('SELECT id FROM towns WHERE starts_with=$1', [regTown]);
+    //console.log(result);
+    let reg = await pool.query('SELECT registration FROM plates WHERE towns_id=$1', [result.rows[0].id]);
+    console.log(reg);
+    return reg.rows;
   }
 
   return {
     takeRegNumber,
     resetDb,
     getRegPlate,
-    forTowns
+    forTowns,
+    allRegNumbers,
+    townFilter
   }
 }
